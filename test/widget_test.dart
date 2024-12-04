332a11e8ad42ac481c3ac9e3c196fc9d3e11644e
Oscar Fernandez Muñoz <ofernandez@sistemaspremium.com>
2024-12-04 17:49:18 -0600

    Initial commit camera access

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,29 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:camera/camera.dart';
 import 'package:hello_world_4/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    // Simula la cámara (puedes usar cualquier objeto de CameraDescription para la prueba)
+    const camera = CameraDescription(
+      name: 'Test Camera',
+      lensDirection: CameraLensDirection.back,
+      sensorOrientation: 0,
+    );
 
-    // Verify that our counter starts at 0.
+    // Construir la aplicación y pasar la cámara simulada.
+    await tester.pumpWidget(MyApp(camera: camera));
+
+    // Verificar que el contador comienza en 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
+    // Tocar el icono '+' y disparar un frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that our counter has incremented.
+    // Verificar que el contador ha incrementado.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
